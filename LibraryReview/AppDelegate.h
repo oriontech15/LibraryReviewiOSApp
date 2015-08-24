3c440f3da5f30e12f3eaa04f640a7736db2863b4
Justin Smith <11jsmith7@gmail.com>
2015-08-24 11:13:57 -0600

    intial Commit

---
+++ LibraryReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibraryReview
+//
+//  Created by Justin Smith on 8/24/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LibraryReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LibraryReview
+//
+//  Created by Justin Smith on 8/24/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
